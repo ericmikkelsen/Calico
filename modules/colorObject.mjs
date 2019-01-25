9c4300d8b78c21f23f82cfdcf505286885239c2b
ercimikkelsen <eric@ericmikkelsen.com>
2019-01-25 04:14:04 +0000

    feat: adds palette to colorObject

--- modules/colorObject.mjs
+++ modules/colorObject.mjs
@@ -2,6 +2,7 @@ import {hexToRGB,RGBToHSL } from './formatColor.mjs'
 import {figureToGround} from './a11y.mjs'
 import lum from './relativeLuminance.mjs'
 import {findNearestNamedColor} from './nearestColor.mjs'
+import paletteHueShift from '../modules/palette'
 
 export default (hex) => {
     const rgb = hexToRGB(hex)
@@ -13,6 +14,7 @@ export default (hex) => {
         white: againstWhite.aaLg ? againstWhite : false ,
         black: againstBlack.aaLg ? againstBlack : false
     }
+    const paletteByHueShift = paletteHueShift(hex)
     return {
         a11y: a11y,
         hex: hex,
@@ -20,5 +22,8 @@ export default (hex) => {
         luminance: lum(hex),
         name: name.name,
         rgb: rgb,
+        palette: {
+            hue: paletteByHueShift
+        }
     }
 }
