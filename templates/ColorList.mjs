d208d2eea6b436b621976aa39f874c63e52beccd
ercimikkelsen <eric@ericmikkelsen.com>
2019-01-25 04:16:41 +0000

    feat: updates colors to be buttons

--- templates/ColorList.mjs
+++ templates/ColorList.mjs
@@ -2,7 +2,7 @@ export default (colors) => {
     const heading = `<h1 style="--color: #${colors[0].hex}">${colors[0].name}</h1>`
     const list = colors.map( color => {
         return `<li id="x${color.hex}" style="--color:#${color.hex}">
-            <b>#${color.hex}</b>
+            <button>#${color.hex}</button>
         </li>`
     })
 return `${heading}<ol>${list.join('')}</ol>`
