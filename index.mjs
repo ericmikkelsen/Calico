fe66fe6da0350cc5d623e862929b2680454466a7
ercimikkelsen <eric@ericmikkelsen.com>
2019-01-25 02:52:27 +0000

    feat: pulls in listed quantum colors

--- index.mjs
+++ index.mjs
@@ -1,20 +1,10 @@
 import {writeFileSync} from 'fs'
 import colorObjects from './modules/colorObjects.mjs'
-import quantumColors from './modules/quantumColors.mjs'
+// import quantumColors from './modules/quantumColors.mjs'
 // templates
 import colorList from './templates/ColorList.mjs'
 import indexByName from './templates/indexByName.mjs'
-
-
-// // contrast
-// console.log( 'contrast between #D5E694 & #E6E694', contrastLevel('D5E694', 'E6E694') )
-
-// // get hue
-// console.log( 'hsl of D5E694', hexToHSL('#D5E694') )
-
-// const hues = hueLuminance()
-//console.log(util.inspect(hues.q.aa, false, null, true /* enable colors */))
-// const hx3 = hex3()
+import q from './data/quantumColors.json'
 
 const q = quantumColors()
 const colors = colorObjects(q)
