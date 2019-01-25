706bfff4b85cc2b22eae0bff04156ba92338fc84
ercimikkelsen <eric@ericmikkelsen.com>
2019-01-25 02:56:14 +0000

    feat: changes dist directory to public
    - updates index.mjs to use public instead of dist for index and individual colors

--- index.mjs
+++ index.mjs
@@ -6,18 +6,16 @@ import colorList from './templates/ColorList.mjs'
 import indexByName from './templates/indexByName.mjs'
 import q from './data/quantumColors.json'
 
-const q = quantumColors()
 const colors = colorObjects(q)
 
 for (const name in colors.byName) {
     const c = Object.values(colors.byName[name])
+    console.log(c);
     const ColorListMarkup = colorList(c)
-
-    writeFileSync(`./dist/${name}.html`, '<link href="../style.css" rel="stylesheet"><body style="--color:'+name+'">'+ColorListMarkup+'</body>')
+    writeFileSync(`./public/${name}.html`, '<link href="./styles/style.css" rel="stylesheet"><body style="--color:'+name+'">'+ColorListMarkup+'<script type="module" src="./modules/frontend/namedColor.mjs"></script></body>')
 }
-
 const indexMarkup = indexByName(Object.keys(colors.byName))
-writeFileSync(`./dist/index.html`, '<link href="../style.css" rel="stylesheet">'+indexMarkup)
+writeFileSync(`./public/index.html`, '<link href="./styles/style.css" rel="stylesheet">'+indexMarkup)
 
 
 
