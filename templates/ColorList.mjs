eae14367358ee145f9e9cc907da9736a2ecf6dcb
ercimikkelsen <eric@ericmikkelsen.com>
2019-03-02 19:54:39 +0000

    feat: the great cull
    - this removes all the markup output from calico
    - moving calico to just being the data and utility functions

--- templates/ColorList.mjs
+++
@@ -1,9 +0,0 @@
-export default (colors) => {
-    const heading = `<h1 style="--color: #${colors[0].hex}">${colors[0].name}</h1>`
-    const list = colors.map( color => {
-        return `<li id="x${color.hex}" style="--color:#${color.hex}">
-            <button>#${color.hex}</button>
-        </li>`
-    })
-return `${heading}<ol>${list.join('')}</ol>`
-}
--- templates/indexByName.mjs
+++
@@ -1,8 +0,0 @@
-export default (colors) => {
-    const list = colors.map( color => {
-        return `<li style="--color:${color}">
-            <a href="${color}.html">${color}</a>
-        </li>`
-    })
-    return `<ul>${list.join('')}</ul>`
-}
